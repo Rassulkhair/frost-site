5061feb6a7c3f9d4e2c441c37957f64cc036f3ff
Rassulkhair <rasulhair@gmail.com>
2022-10-02 10:59:20 +0600

    Frost

---
+++ js/script.js
@@ -0,0 +1,10 @@
+tns({
+  container: '.slider-content',
+  mouseDrag: true,
+  prevButton: '.prev',
+  nextButton: '.next',
+  nav: false,
+  autoplayButtonOutput: false,
+  autoplay: false,
+  autoplayTimeout: 10000,
+});
